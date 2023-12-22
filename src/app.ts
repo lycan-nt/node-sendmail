da49361bcd5d683d41a4dd85372928f9eb067463
Felipe D. Santos <lycan.felipe@gmail.com>
2023-12-22 20:18:47 -0300

    SwaggerAPI

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { router } from './routes';
+import  swaggerUI  from "swagger-ui-express";
+import swaggerDocument from '../swagger.json';
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 export { app }
